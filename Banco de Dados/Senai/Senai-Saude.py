0c3f33174d11ae7167e3c5228119fcff9b9a7a5e
Alexandre Victor <169073432+halliontk@users.noreply.github.com>
2024-06-14 20:39:38 -0300

    Update Senai-Saude.py

--- Banco de Dados/Senai/Senai-Saude.py
+++ Banco de Dados/Senai/Senai-Saude.py
@@ -106,7 +106,7 @@ if op_menu == 1:
 
                     if op_exclusao == 1:
                         sql = "DELETE FROM PACIENTE WHERE ID = %s"
-                        cursor.execute(sql, (id_paciente))
+                        cursor.execute(sql, (id_paciente,))
                         conexao.commit()
                         input(f"\nPciente {nome_paciente} excluído\nPressione enter para continuar.")
 
@@ -120,10 +120,10 @@ if op_menu == 1:
             except Exception as e:
                 print("Erro ", e)
 
-
 elif op_menu == 2:
     print("-=-= ÁREA DE MÉDICOS =-=-")
     op_menu_sec = int(input(("1. Cadastrar\n2. Editar\n3. Consultar\n4. Excluir\n\nEscolha uma opção: ")))
+    limpar_tela()
 
     match op_menu_sec:
         case 1:
@@ -197,7 +197,117 @@ elif op_menu == 2:
 
                         except Exception as e:
                             print(f"Erro: {e}")
+        case 4:
+            print("-=-= EXCLUSÃO DE MÉDICO =-=-\n")
+            cpf = input("CRM: ")
 
+            try:
+                sql = "SELECT ID, NOME FROM MEDICO WHERE CRM = %s"
+                cursor.execute(sql, (cpf,))
+                resultado = cursor.fetchall()  # retorna para resultado; todas as linhas retornadas pela execução da query
+
+                if len(resultado) == 0:
+                    print("Médico não encontrado.")
+                    delay()
+
+                else:
+                    id_medico = resultado[0][0]
+                    nome_medico = resultado[0][1]
+
+                    op_exclusao = int(
+                        input(f"\nDeseja excluir o médico {nome_medico}?\n1. Sim\n2. Não\n\nEscolha uma opção: "))
+
+                    if op_exclusao == 1:
+                        sql = "DELETE FROM MEDICO WHERE ID = %s"
+                        cursor.execute(sql, (id_medico,))
+                        conexao.commit()
+                        input(f"\nPaciente {nome_medico} excluído\nPressione enter para continuar.")
+
+                    elif op_exclusao == 2:
+                        print("Retornando ao menu principal")
+                        delay()
+
+                    else:
+                        print("Opção inválida.")
+                        delay()
+            except Exception as e:
+                print("Erro ", e)
 
     #case _:  # DEFAULT = ELSE
      #   print("Opção Inválida")
+
+elif op_menu == 3:
+    print("-=-= ÁREA DE CONSULTAS =-=-")
+    op_menu_sec = int(input(("1. Cadastrar\n2. Editar\n3. Visualizar\n4. Excluir\n\nEscolha uma opção: ")))
+    limpar_tela()
+
+    match op_menu_sec:
+        case 1:
+            print("-=-= CADASTRO DE CONSULTA =-=-\n")
+
+            while True:
+                crm_medico = input("CRM do Médico: ")
+
+                sql = "SELECT ID FROM MEDICO WHERE CRM = %s"
+                cursor.execute(sql, (crm_medico,))
+                resultado = cursor.fetchall()
+
+                if len(resultado) == 0:
+                    print("Médico não encontrado!")
+                    delay()
+
+                else:
+                    id_medico = resultado[0][0]
+
+                    sql = "SELECT STATUS_MEDICO FROM MEDICO WHERE ID = %s"
+                    cursor.execute(sql, (id_medico,))
+                    resultado = cursor.fetchall()
+                    status_medico = resultado[0][0]
+
+                    if status_medico == 'Inativo':
+                        print("Não é possível marcar consulta para este médico, pois ele possui status Inativo!")
+                        delay()
+
+                    else:
+                        cpf_paciente = input("CPF do paciente: ")
+
+                        sql = "SELECT ID FROM PACIENTE WHERE CPF = %s"
+                        cursor.execute(sql, (cpf_paciente,))
+                        resultado = cursor.fetchall()
+
+                        if len(resultado) == 0:
+                            limpar_tela()
+                            print("Paciente não encontrado!")
+                            delay()
+                            limpar_tela()
+
+                        else:
+                            id_paciente = resultado[0][0]
+                            cod_consulta = input("Código da Consulta: ")
+
+                            sql = "SELECT ID FROM CONSULTA WHERE COD_CONSULTA = %s"
+                            cursor.execute(sql, (cod_consulta,))
+                            resultado = cursor.fetchall()
+
+                            if len(resultado) != 0:
+                                limpar_tela()
+                                print("Já existe consulta cadastrada com este código! Tente novamente...")
+                                delay()
+
+                            else:
+                                dt_consulta = input("Data da Consulta (YYYY-MM-DD): ")
+                                hr_consulta = input("Hora da Consulta (HH:MM:SS): ")
+                                # Adicionar tratamento para inserção de DATA e HORA
+                                # Adicionar validação para DATA E HORA disponíveis (medico e paciente)
+
+                                vr_consulta = float(input("Valor da Consulta: "))
+
+                                sql = '''INSERT INTO CONSULTA(COD_CONSULTA, DT_CONSULTA, HR_CONSULTA, 
+                                            VR_CONSULTA, ID_MEDICO, ID_PACIENTE) VALUES (%s, %s, %s, %s, %s, %s)'''
+                                valores = (cod_consulta, dt_consulta, hr_consulta, vr_consulta, id_medico, id_paciente)
+
+                                cursor.execute(sql, valores)
+                                conexao.commit()
+                                print("Consulta cadastrada com sucesso!")
+
+                                # INSERIR ESTRUTURA TRY-EXCEPT
